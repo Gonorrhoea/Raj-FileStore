2466dcdbdae84dfd53efef86d7b7e56a1f3b5d71
Gonorrhoea <135523662+Gonorrhoea@users.noreply.github.com>
2024-09-23 09:55:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,6 @@
+import pyrogram.utils
+
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 from aiohttp import web
 from plugins import web_server
@@ -54,8 +57,8 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/codeflix_bots")
-        self.LOGGER(__name__).info(f""" BOT DEPLOYED BY @CODEFLIX_BOTS""")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/FilmyFundas")
+        self.LOGGER(__name__).info(f""" BOT DEPLOYED BY @FilmyFundas""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
